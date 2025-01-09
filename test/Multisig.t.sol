2a5b430d74653b18dfe16882f035afd34496117a
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2025-01-09 11:47:59 +0100

    finalize test

--- test/Multisig.t.sol
+++ test/Multisig.t.sol
@@ -6,6 +6,7 @@ import "../src/Multisig.sol";
 
 contract MultisigTest is Test {
     Multisig public multisig;
+    Multisig public multisigFail;
 
     address[] public signers;
     address public signer1 = address(0x1);
@@ -13,6 +14,7 @@ contract MultisigTest is Test {
     address public signer3 = address(0x3);
     address public signer4 = address(0x4);
     address public nonSigner = address(0x5);
+    address public signer0 = address(0x0);
 
     function setUp() public {
         // Initialize signers
@@ -21,6 +23,29 @@ contract MultisigTest is Test {
         // Deploy Multisig contract with 3 signers and 2 confirmations required
         multisig = new Multisig(signers, 2);
         vm.deal(address(multisig), 5 ether); // Fund contract with 5 ether
+    }
+
+    function testMinimal() public {        
+        signers = [signer1];
+        vm.expectRevert("At least 3 signers required");
+        multisigFail = new Multisig(signers,2);
+    }
+
+    function testMinimal2() public {
+        vm.expectRevert("Invalid required confirmations");
+        multisigFail = new Multisig(signers,1);
+    }
+
+    function testMinimal3() public {    
+        signers = [signer0,signer1,signer2];
+        vm.expectRevert("Invalid signer address");
+        multisigFail = new Multisig(signers,2);
+    }
+
+    function testMinimal4() public {    
+        signers = [signer1,signer2,signer1];
+        vm.expectRevert("Signer not unique");
+        multisigFail = new Multisig(signers,2);
     }
 
     function testSignersInitialization() public {
@@ -102,8 +127,15 @@ contract MultisigTest is Test {
 
     function testInvalidTransactionExecution() public {
         // Try executing a non-existent transaction
-        vm.expectRevert("Transaction does not exist");
         vm.prank(signer1);
+        vm.expectRevert("Transaction does not exist");
+        multisig.confirmTransaction(0);
+    }
+
+    function testInvalidTransactionExecution2() public {
+        // Try executing a non-existent transaction
+        vm.prank(signer1);
+        vm.expectRevert("Transaction does not exist");
         multisig.executeTransaction(0);
     }
 
@@ -115,13 +147,35 @@ contract MultisigTest is Test {
         multisig.submitTransaction(address(0x5), 1 ether, "0x0");
     }
 
-    function testNotASigner() public {
+    function testSigner() public {
         // Attempt to submit a transaction by a non-signer
-        vm.expectRevert("Not a signer");
         vm.prank(nonSigner);
+        vm.expectRevert("Not a signer");
         multisig.submitTransaction(nonSigner, 1 ether, "0x0");
     }
 
+    function testSigner2() public {
+        // Attempt to submit a transaction by a non-signer
+        vm.prank(signer1);
+        multisig.submitTransaction(signer1, 1 ether, "0x0");
+        vm.prank(nonSigner);
+        vm.expectRevert("Not a signer");
+        multisig.confirmTransaction(0);
+    }
+
+    function testSigner3() public {
+        // Attempt to submit a transaction by a non-signer
+        vm.prank(nonSigner);
+        vm.expectRevert("Not a signer");
+        multisig.addSigner(signer4);
+    }
+
+    function testSigner4() public {
+        // Attempt to submit a transaction by a non-signer
+        vm.prank(nonSigner);
+        vm.expectRevert("Not a signer");
+        multisig.removeSigner(signer1);
+    }
     function testAlreadyExecutedTransaction() public {
         // Submit and execute a transaction
         vm.prank(signer1);
@@ -148,7 +202,36 @@ contract MultisigTest is Test {
         vm.prank(signer3);
         vm.expectRevert("Transaction failed");
         multisig.confirmTransaction(0);
+    }
 
-        // Execute should fail
+    function testExecutionNotConfirmed() public {
+        vm.startBroadcast(signer1);
+        multisig.submitTransaction(address(signer1), 10 ether, "0x0");
+        vm.expectRevert("Insufficient confirmations");
+        multisig.executeTransaction(0);
+    }
+
+    function testAddFakeSigner() public {
+        vm.prank(signer1);
+        vm.expectRevert("Invalid address");
+        multisig.addSigner(signer0);
+    }
+
+    function testAlreadySigner() public {
+        vm.prank(signer1);
+        vm.expectRevert("Already a signer");
+        multisig.addSigner(signer1);
+    }
+
+    function testRemoveNonSigner() public {
+        vm.prank(signer1);
+        vm.expectRevert("Not a signer");
+        multisig.removeSigner(nonSigner);
+    }
+
+    function testRemoveNotEnough() public {
+        vm.prank(signer1);
+        vm.expectRevert("Cannot have less than 3 signers");
+        multisig.removeSigner(signer3);
     }
 }
